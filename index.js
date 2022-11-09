54e43a36f67863e6f6cb5583a5ddb3bb5480fc47
Alysson Marley Dias <callistomain@gmail.com>
2022-11-09 00:10:05 -0300

    Testing Back~DB

--- index.js
+++ index.js
@@ -9,11 +9,44 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const client = new MongoClient("mongodb://localhost:27017");
-let db;
+const client = new MongoClient(process.env.MONGO_URI);
+let db, participants, messages, status;
 
 client.connect().then(() => {
-	db = client.db("db-name");
+	db = client.db("batePapoTest");
+    participants = db.collection("participants");
+    messages = db.collection("messages");
+    status = db.collection("status");
+});
+
+app.get("/participants", async (req, res) => {
+    try {
+        const documents = await participants.find().toArray();
+        res.send(documents);
+    } catch (e) {
+        console.log('Error: ', e.message);
+        res.sendStatus(500);
+    }
+});
+
+app.post("/participants", async (req, res) => {
+    try {
+        const result = await participants.insertOne(req.body);
+        res.send(result);
+    } catch (e) {
+        console.log('Error: ', e.message);
+        res.sendStatus(500);
+    }
+});
+
+app.post("/participantss", async (req, res) => {
+    try {
+        const result = await participants.updateOne({name:"wut"}, { $set: {"name": "waaat"} });
+        res.send(result);
+    } catch (e) {
+        console.log('Error: ', e.message);
+        res.sendStatus(500);
+    }
 });
 
 app.listen(port, () => {
